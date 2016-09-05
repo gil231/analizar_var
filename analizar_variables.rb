0de45bcf609a92762aa406ccb7751d7b62d16f39
gil <gil@MacBook-Pro-de-gil.local>
2016-09-05 17:13:19 -0600

    analizar variables

---
+++ analizar_variables.rb
@@ -0,0 +1,17 @@
+# suma_uno = 4 + 7
+# suma_dos = suma_uno + 8
+# suma_dos == 19
+# true
+
+resta_uno = 3 -2
+valor = "uno"
+resta_dos = resta_uno - valor
+resta_dos == 0
+
+
+# multiplicacion = 6 * 5
+# division = multiplicacion / 5
+# operacion = division - 3
+# cadena = "Hola" * operacion
+# cadena == "HolaHolaHolaHola"
+# false
